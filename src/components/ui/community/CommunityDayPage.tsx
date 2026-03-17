84fccac84c53b45329d4eab458e267e9504cf907
Ivan Gaton <gaton.ivan@gmail.com>
2026-03-17 10:31:03 -0400

    feat: add second image to community day page

--- src/components/ui/community/CommunityDayPage.tsx
+++ src/components/ui/community/CommunityDayPage.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router'
 import { useBottomScroll } from '../../../hooks/useBottomScroll'
 import { SubPageWrapper } from './SubPageWrapper'
 import communityDaySecondImg from '../../../assets/community/community_day_second.jpeg'
+import communityDayThirdImg from '../../../assets/community/community_day_third.jpeg'
 import './community-sub.css'
 
 export function CommunityDayPage() {
@@ -26,6 +27,7 @@ export function CommunityDayPage() {
           levels participated in tennis &amp; soccer. Community Day raised
           awareness of maintaining vital open spaces for gathering.
         </p>
+        <img className="community-sub-img" src={communityDayThirdImg} alt="Community Day" style={{ marginTop: '1.5rem' }} />
       </div>
     </SubPageWrapper>
   )
